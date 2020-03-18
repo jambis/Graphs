5996df0ee6e77def273368a7b2154f4ece2c340a
jambis <imjaamees@gmail.com>
2020-03-18 12:39:42 -0500

    MVP for ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,35 @@
+from util import Stack
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    #[(Parent,Child)]
+    s = Stack()
+    s.push([starting_node])
+
+    longest_path = []
+    
+    visited = set()
+
+    while s.size() > 0:
+        path = s.pop()
+        node = path[-1]
+
+        if len(path) > len(longest_path):
+            longest_path = path
+        
+        if len(path) == len(longest_path) and path[-1] < longest_path[-1]:
+            longest_path = path
+
+        if node not in visited:
+            visited.add(node)
+
+            for pair in ancestors:
+                if pair[1] == node:
+                    new_path = [*path, pair[0]]
+                    s.push(new_path)
+
+    if longest_path[-1] == starting_node:
+        return -1
+
+    return longest_path[-1]
+    
+    
